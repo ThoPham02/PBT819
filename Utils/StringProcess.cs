2bf1e92046a4e66268a2c27042de4107c7fd815a
ThoPham02 <2021050819@student.humg.edu.vn>
2024-01-02 15:17:11 +0700

    final commit

---
+++ Utils/StringProcess.cs
@@ -0,0 +1,7 @@
+namespace PBT819.Utils {
+    public static class StringProcess {
+        public static string LowerToUpper(string input) {
+            return input.ToUpper();
+        }
+    }
+}
--- obj/Debug/net8.0/PBT819.AssemblyInfo.cs
+++ obj/Debug/net8.0/PBT819.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PBT819")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b61acec73c560d82a8cef04ed700cecb69d6468")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67fb17190ab01e1892c5720ac8f176e98ac0c2f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PBT819")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PBT819")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
